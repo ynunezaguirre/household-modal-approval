b79d40a08f5112874f91b4409271083a47726104
yamil <yamil@skysite.com.ec>
2022-10-22 17:24:05 -0500

    add income widget components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as InitWidget } from "./InitWidget";
 export { default as AssetsWidget } from "./AssetsWidget";
 export { default as MoffinWidget } from "./MoffinWidget";
+export { default as IncomeWidget } from "./IncomeWidget";
